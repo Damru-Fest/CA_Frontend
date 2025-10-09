bbe0fdcc1dd0b677d0a4afb89f69b24c4fd01cac
Nimit Sharma <91617550+ABUDIYAAAA@users.noreply.github.com>
2025-10-09 11:57:11 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,42 @@ export const metadata: Metadata = {
   description:
     "Join the Damru Campus Ambassador Program and be a part of an exciting journey to promote the Damru Fest at your college. As a campus ambassador, you'll have the opportunity to develop your leadership skills, gain valuable experience, and connect with like-minded individuals. Sign up now to become a key player in making the Damru Fest a memorable event on your campus!",
   icons: {
-    icon: "/favicon/favicon.ico",
+    icon: [
+      {
+        url: "/favicon/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon/favicon.svg",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon/favicon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+    ],
+    apple: {
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
