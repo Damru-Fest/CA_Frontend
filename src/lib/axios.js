ebb95c140a3eefc46f2c2c1df1a92f5d651f5593
Kevish Sewliya <prokevish07@gmail.com>
2025-10-22 01:33:05 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// Create an axios instance with default configuration
+
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true, // Always send cookies with requests
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
